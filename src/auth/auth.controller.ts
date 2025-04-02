818a1ba405d8b2bad2457f3062d07a0042fdd610
Carlos Motino <carlos.motino911@gmail.com>
2025-04-02 14:06:04 -0600

    Fin seccion 12

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,38 @@
+import { Body, Controller, Get, Inject, Post, UseGuards } from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { NATS_SERVICE } from 'src/config';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { LoginUserDto } from './dto';
+import { catchError } from 'rxjs';
+import { AuthGuard } from './guards/auth.guards';
+import { Token, User } from './decorators';
+import { CurrentUser } from './interfaces/current-user.interface';
+
+@Controller('auth')
+export class AuthController {
+  constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
+
+  @Post('register')
+  register(@Body() registerUserDto: RegisterUserDto) {
+    return this.client.send('auth.register.user', registerUserDto).pipe(
+      catchError((error) => {
+        throw new RpcException(error);
+      }),
+    );
+  }
+
+  @Post('login')
+  login(@Body() loginUserDto: LoginUserDto) {
+    return this.client.send('auth.login.user', loginUserDto).pipe(
+      catchError((error) => {
+        throw new RpcException(error);
+      }),
+    );
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('verify')
+  verify(@User() user: CurrentUser, @Token() token: string) {
+    return { user, token };
+  }
+}
